ff30c0def68c77cf7c8bee3c4830ec978db39376
Muhammed Jazil <jazilwork7@gmail.com>
2026-04-13 07:50:24 +0530

    Fix CORS and allowed headers for hosted environment

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    allowedHeaders: ['Content-Type', 'x-user-id'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
